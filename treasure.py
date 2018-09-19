a3c0ddf3b0e0dd36a275aba1325fde4de8e80df4
drpriver <priver.david@gmail.com>
2018-09-18 21:11:13 -0400

    Parses to ints on load instead of on use

--- treasure.py
+++ treasure.py
@@ -13,16 +13,16 @@ class treasure_table:
             rows = [r for r in treasure_reader]
         treasure = {}
         for row in rows:
-            tr = treasure_row(*row)
+            tr = treasure_row(*(int(x) for x in row))
             treasure[int(tr.Level)] = tr
         self.treasure = treasure
     def __call__(self, dungeon_level):
         treasure_row = self.treasure[dungeon_level]
-        gold = roll_gold(int(treasure_row.Gold))
-        silver = roll_silver(int(treasure_row.Silver), dungeon_level)
-        gems = roll_gems(int(treasure_row.Gems), dungeon_level)
-        jewels = roll_jewels(int(treasure_row.Jewels), dungeon_level)
-        magic = roll_magic(int(treasure_row.Magic))
+        gold = roll_gold(treasure_row.Gold)
+        silver = roll_silver(treasure_row.Silver, dungeon_level)
+        gems = roll_gems(treasure_row.Gems, dungeon_level)
+        jewels = roll_jewels(treasure_row.Jewels, dungeon_level)
+        magic = roll_magic(treasure_row.Magic)
         return gold, silver, gems, jewels, magic, dungeon_level
 
 def roll_gold(Gold_constant):
